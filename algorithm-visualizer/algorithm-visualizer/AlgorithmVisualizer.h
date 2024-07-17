79428d474357869ed7c12d0da8a37eea1c151b9f
Jung Yeonsu <jos6151@naver.com>
2024-07-17 11:52:49 +0900

    Algorithm Visualizer header

---
+++ algorithm-visualizer/algorithm-visualizer/AlgorithmVisualizer.h
@@ -0,0 +1,34 @@
+#ifndef ALGORITHM_VISUALIZER_H
+#define ALGORITHM_VISUALIZER_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+enum class Algorithm {
+	BubbleSort,
+	QuickSort,
+	InsertionSort,
+	SelectionSort
+};
+
+class AlgorithmVisualizer {
+public:
+	AlgorithmVisualizer();
+	void update();
+	void render(sf::RenderWindow& window);
+	void setAlgorithm(Algorithm algo);
+	
+private:
+	std::vector<int> data;
+	Algorithm currentAlgorithm;
+	void initializeData();
+	void bubbleSortStep();
+	void quickSortStep();
+	void insertionSortStep();
+	void selectionSortStep();
+	int partition(int low, int high);
+	void quickSort(int low, int high);
+	bool sorted;
+};
+
+#endif
---
+++ algorithm-visualizer/algorithm-visualizer/main.cpp
@@ -0,0 +1,6 @@
+#include <SFML/Graphics.hpp>
+#include "AlgorithmVisualizer.h"
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Algorithm Visualizer");
+}
