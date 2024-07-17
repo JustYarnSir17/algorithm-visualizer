ffa431e9c5305c0d9d8d536cee22a89d0c96a4de
Jung Yeonsu <jos6151@naver.com>
2024-07-17 12:05:50 +0900

    add main

--- algorithm-visualizer/algorithm-visualizer/main.cpp
+++ algorithm-visualizer/algorithm-visualizer/main.cpp
@@ -8,4 +8,28 @@ int main() {
 	visualizer.setAlgorithm(Algorithm::BubbleSort);
 
 
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Num1)
+                    visualizer.setAlgorithm(Algorithm::BubbleSort);
+                else if (event.key.code == sf::Keyboard::Num2)
+                    visualizer.setAlgorithm(Algorithm::QuickSort);
+                else if (event.key.code == sf::Keyboard::Num3)
+                    visualizer.setAlgorithm(Algorithm::InsertionSort);
+                else if (event.key.code == sf::Keyboard::Num4)
+                    visualizer.setAlgorithm(Algorithm::SelectionSort);
+            }
+        }
+
+        visualizer.update();
+        window.clear();
+        visualizer.render(window);
+        window.display();
+    }
+
+    return 0;
 }
